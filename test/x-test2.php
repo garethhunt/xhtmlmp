fbde5cae9eaf00b77f8875cb45b77de1ff38cdf6
gareth <gareth>
2010-05-22 20:59:39 +0000

    Test HTML files for XHTML-MP

---
+++ test/x-test2.php
@@ -0,0 +1,16 @@
+<?php
+  header("Content-Type: application/vnd.wap.xhtml+xml");
+?><!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN"
+"http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Hello</title>
+  </head>
+  <body>
+    <p>This is a basic XHTML Mobile Profile 1.0 page.  You should see <strong>[XHTML-MP]</strong> in the tab title.</p>
+    <ul>
+      <li>This test has the XML declaration missing.</li>
+    </ul>
+    <p><a href="index.html">Back to menu</a></p>
+  </body>
+</html>
