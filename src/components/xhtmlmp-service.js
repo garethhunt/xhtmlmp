2e71de1447bbf26fd12bffbf046a1a06025595d9
Gareth Hunt <gareth-hunt@rocketmail.com>
2011-01-15 12:52:19 -0800

    Enabled xhtmlmp-service.js for Firefox 4 compatibility.

--- src/components/xhtmlmp-service.js
+++ src/components/xhtmlmp-service.js
@@ -25,163 +25,105 @@
  */
 
 /* application/vnd.wap.xhtml+xml -> text/html stream converter */
-function XHTMLMPStreamConverter () {}
+if (!XHTMLMP)
+	var XHTMLMP = {};
 
-XHTMLMPStreamConverter.prototype = {
+if (!XHTMLMP.StreamConverter) {
+	Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+	
+	XHTMLMP.StreamConverter = function () {};
+	XHTMLMP.StreamConverter.prototype = {
+		classDescription: "XHTMLMP to HTML stream converter",
+		classID:          Components.ID("{333c7e10-e060-4514-9e35-08d2183aee2d}"),
+		contractID:       "@mozilla.org/streamconv;1?from=application/vnd.wap.xhtml+xml&to=*/*",
+		
+		_xpcom_factory: {
+			createInstance: function (outer, iid) {
+				if (outer != null)
+					throw Components.results.NS_ERROR_NO_AGGREGATION;
+			
+				if (iid.equals(Components.interfaces.nsISupports) ||
+					iid.equals(Components.interfaces.nsIStreamConverter) ||
+					iid.equals(Components.interfaces.nsIStreamListener) ||
+					iid.equals(Components.interfaces.nsIRequestObserver)) {
+					return new XHTMLMP.StreamConverter();
+				}
+				throw Components.results.NS_ERROR_NO_INTERFACE;
+			}
+		},
+		
+		QueryInterface: XPCOMUtils.generateQI (
+			[Components.interfaces.nsIObserver,
+			Components.interfaces.nsIStreamConverter,
+			Components.interfaces.nsIStreamListener,
+			Components.interfaces.nsIRequestObserver]
+		),
+		
+		// nsIRequest::onStartRequest
+		onStartRequest: function(aRequest, aContext) {
+			this.data = "";
+			this.uri = aRequest.QueryInterface (Components.interfaces.nsIChannel).URI.spec;
+		    this.channel = aRequest;
+		    this.channel.contentType = "text/xml";
+		    this.listener.onStartRequest (this.channel, aContext);
+		},
+		
+		// nsIRequest::onStopRequest
+		onStopRequest: function (aRequest, aContext, aStatusCode) {
+		    var tempData = this.data;
 
-  _logger: null,
-  _initialised: false,
-  
-  init: function() {
-    if (this._initialised) { return }
-    this._logger = Components.classes["@xhtmlmp.mozdev.org/logger;1"].getService(Components.interfaces.nsISupports).wrappedJSObject
-  },
-  
-  QueryInterface: function (iid) {
+		    // Strip leading whitespace
+		    tempData = tempData.replace (/^\s+/,'');
+		    
+		    // Strip out comments
+		    tempData = tempData.replace (/<!(?:--.*?--\s*)?>/,'');
+		    
+		    // Replace <html> with <html xmlns="http://www.w3.org/1999/xhtml">
+		    tempData = tempData.replace (/<html>/,'<html xmlns="http://www.w3.org/1999/xhtml">');
+		    
+		    // Prepend </title> with [XHTML-MP]
+		    tempData = tempData.replace (/<\/title>/,' [XHTML-MP]</title>');
+		    
+		    this.data = tempData;
+		    
+		    var targetDocument = "";
+		    
+		    // TODO Review and determine if this is required
+		    // If there is no XML declaration, add it
+		    /* if ( !(this.data.search(/^<\?xml version=['"]1.0['"] encoding=['"]UTF-8['"]\?>.*$/)) ) {
+		    	targetDocument = "<?xml version='1.0' encoding='UTF-8'?>";
+		    } */
+		    
+		    var targetDocument = targetDocument + this.data;
+		    
+		    var sis = Components.classes["@mozilla.org/io/string-input-stream;1"].createInstance(Components.interfaces.nsIStringInputStream);
+		    sis.setData (targetDocument, targetDocument.length);
 
-    if (iid.equals(Components.interfaces.nsISupports) ||
-      iid.equals(Components.interfaces.nsIStreamConverter) ||
-      iid.equals(Components.interfaces.nsIStreamListener) ||
-      iid.equals(Components.interfaces.nsIRequestObserver))
-      return this;
-
-    throw Components.results.NS_ERROR_NO_INTERFACE
-  },
-
-  // nsIRequest::onStartRequest
-  onStartRequest: function(aRequest, aContext) {
-    if (!this._initialised) { this.init() }
-    this._logger.debug("Entered onStartRequest")
-
-    this.data = ''
-
-    this.uri = aRequest.QueryInterface (Components.interfaces.nsIChannel).URI.spec
-
-    this.channel = aRequest
-    this._logger.debug(this.channel.contentType)
-    
-    this.channel.contentType = "text/xml"
-    this._logger.debug(this.channel.contentType)
-
-    this.listener.onStartRequest (this.channel, aContext)
-    this._logger.debug("Exiting onStartRequest")
-  },
-
-  // nsIRequest::onStopRequest
-  onStopRequest: function (aRequest, aContext, aStatusCode) {
-    this._logger.debug("this.data: " + this.data)
-    var tempData = this.data
-
-    // Strip leading whitespace
-    tempData = tempData.replace (/^\s+/,'')
-    
-    // Strip out comments
-    tempData = tempData.replace (/<!(?:--.*?--\s*)?>/,'')
-    
-    // Replace <html> with <html xmlns="http://www.w3.org/1999/xhtml">
-    tempData = tempData.replace (/<html>/,'<html xmlns="http://www.w3.org/1999/xhtml">')
-    
-    // Prepend </title> with [XHTML-MP]
-    tempData = tempData.replace (/<\/title>/,' [XHTML-MP]</title>')
-    
-    this.data = tempData
-    this._logger.debug("this.data: " + this.data)
-    
-    var targetDocument = ""
-    
-    // If there is no XML declaration, add it
-    /* if ( !(this.data.search(/^<\?xml version=['"]1.0['"] encoding=['"]UTF-8['"]\?>.*$/)) ) {
-    	targetDocument = "<?xml version='1.0' encoding='UTF-8'?>"
-    } */
-    
-    var targetDocument = targetDocument + this.data
-    
-    this._logger.debug("targetDocument: " + targetDocument)
-
-    var sis = Components.classes["@mozilla.org/io/string-input-stream;1"].createInstance(Components.interfaces.nsIStringInputStream)
-    sis.setData (targetDocument, targetDocument.length)
-
-    // Pass the data to the main content listener
-    this.listener.onDataAvailable (this.channel, aContext, sis, 0, targetDocument.length)
-    this.listener.onStopRequest (this.channel, aContext, aStatusCode)
-  },
-
-  // nsIStreamListener methods
-  onDataAvailable: function (aRequest, aContext, aInputStream, aOffset, aCount) {
-    this._logger.debug("Entered onDataAvailable")
-    
-    var si = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance()
-    si = si.QueryInterface(Components.interfaces.nsIScriptableInputStream)
-    si.init(aInputStream)
-    this.data += si.read(aCount)
-    
-    this._logger.debug("Exiting onDataAvailable")
-  },
-  
-  asyncConvertData: function (aFromType, aToType, aListener, aCtxt) {
-    // Store the listener passed to us
-    this.listener = aListener
-  },
-  
-  convert: function (aFromStream, aFromType, aToType, aCtxt) {
-    return aFromStream
-  }
+		    // Pass the data to the main content listener
+		    this.listener.onDataAvailable (this.channel, aContext, sis, 0, targetDocument.length);
+		    this.listener.onStopRequest (this.channel, aContext, aStatusCode);
+		},
+		
+		// nsIStreamListener methods
+		onDataAvailable: function (aRequest, aContext, aInputStream, aOffset, aCount) {
+			var si = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance();
+			si = si.QueryInterface(Components.interfaces.nsIScriptableInputStream);
+			si.init(aInputStream);
+			this.data += si.read(aCount);
+		},
+		
+		asyncConvertData: function (aFromType, aToType, aListener, aCtxt) {
+		    // Store the listener passed to us
+		    this.listener = aListener;
+		},
+		  
+		convert: function (aFromStream, aFromType, aToType, aCtxt) {
+		    return aFromStream;
+		}
+	};
 }
 
-var XHTMLMPBrowserModule = {
-
-  cid: Components.ID("{333c7e10-e060-4514-9e35-08d2183aee2d}"),
-  conversion: "?from=application/vnd.wap.xhtml+xml&to=*/*",
-  contractID: "@mozilla.org/streamconv;1",
-  name: "XHTMLMP to HTML stream converter",
-
-  // This factory attribute returns an anonymous class 
-  factory: {
-    createInstance: function (outer, iid) {
-      if (outer != null)
-        throw Components.results.NS_ERROR_NO_AGGREGATION
-      
-      if (iid.equals(Components.interfaces.nsISupports) ||
-        iid.equals(Components.interfaces.nsIStreamConverter) ||
-        iid.equals(Components.interfaces.nsIStreamListener) ||
-        iid.equals(Components.interfaces.nsIRequestObserver)) {
-        
-        return new XHTMLMPStreamConverter()
-      }
-      
-      throw Components.results.NS_ERROR_NO_INTERFACE
-    }
-  },
-
-  registerSelf: function (compMgr, fileSpec, location, type) {
-
-    compMgr = compMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar)
-    compMgr.registerFactoryLocation(this.cid, this.name, this.contractID + this.conversion, fileSpec, location, type)
-
-    var catman = Components.classes["@mozilla.org/categorymanager;1"].getService(Components.interfaces.nsICategoryManager)
-    catman.addCategoryEntry(this.contractID, this.conversion, this.name, true, true)
-  },
-
-  unregisterSelf: function(compMgr, fileSpec, location) {
-    aCompMgr = aCompMgr.QueryInterface(Components.interfaces.nsIComponentRegistrar)
-    aCompMgr.unregisterFactoryLocation(this.cid, aLocation)
-  },
-
-  getClassObject: function (compMgr, cid, iid) {
-
-    if (cid.equals(this.cid))
-      return this.factory
-
-    if (!iid.equals(Components.interfaces.nsIFactory))
-      throw Components.results.NS_ERROR_NOT_IMPLEMENTED
-
-    throw Components.results.NS_ERROR_NO_INTERFACE
-  },
-
-  canUnload: function(compMgr) { return true }
-}
-
-/* entrypoint */
-function NSGetModule(compMgr, fileSpec) {
-    return XHTMLMPBrowserModule
-}
+if (XPCOMUtils.generateNSGetFactory)
+    var NSGetFactory = XPCOMUtils.generateNSGetFactory([XHTMLMP.StreamConverter]);
+else
+    var NSGetModule = XPCOMUtils.generateNSGetModule([XHTMLMP.StreamConverter]);
