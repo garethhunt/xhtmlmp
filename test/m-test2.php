ca9700eab24327aa9b311121198c675645e04e74
Gareth Hunt <gareth-hunt@rocketmail.com>
2011-01-15 12:59:26 -0800

    Added multipart test 2

--- test/m-test2.php
+++ test/m-test2.php
@@ -1,10 +1,11 @@
 <?php
-header("Content-Type: multipart/mixed; boundary=part1x");
+header("Content-Type: multipart/mixed; boundary=part2x");
 ?>
 
---part1x
+
+--part2x
 Content-Type: text/html
-Content-Location: m-test1.php
+Content-Location: m-test2.php
 Content-Transfer-Encoding: binary
 
 <html>
@@ -13,18 +14,21 @@ Content-Transfer-Encoding: binary
   </head>
   <body>
     <!-- The content should be output without barfing on this comment -->
-    <div>The Firefox image below is part of this Multipart package.</div>
+    <p>The Firefox image below is part of this Multipart package.</p>
     <div>
       <img src="product-firefox.gif" alt="Mozilla Firefox" />
     </div>
+    <p>This test has additional newlines before the part delimiter to test bug <a href="https://www.mozdev.org/bugs/show_bug.cgi?id=22002">https://www.mozdev.org/bugs/show_bug.cgi?id=22002</a></p>
   </body>
 </html>
 
---part1x
+
+--part2x
 Content-Type: image/gif
 Content-Location: product-firefox.gif
 Content-Transfer-Encoding: binary
 
 <?php include "product-firefox.gif" ?>
 
---part1x--
+
+--part2x--
